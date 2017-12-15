c1c0afa09e32924e1ca49b67c75e41e985ee7644
kuisatz <kuisatzhaderah@gmail.com>
2017-12-15 11:14:59 +0300

    $languageIdValue

--- vendor/dal/pdo/MblLogin.php
+++ vendor/dal/pdo/MblLogin.php
@@ -8561,7 +8561,7 @@ class MblLogin extends \DAL\DalSlim {
                         '00000000-0000-0000-0000-000000000000' as SinifID  
                     FROM [BILSANET_MOBILE].[dbo].[sys_specific_definitions] a
                     INNER JOIN BILSANET_MOBILE.dbo.sys_language l ON l.id = 647 AND l.deleted =0 AND l.active =0 
-                    LEFT JOIN BILSANET_MOBILE.dbo.sys_language lx ON lx.id =647 AND lx.deleted =0 AND lx.active =0
+                    LEFT JOIN BILSANET_MOBILE.dbo.sys_language lx ON lx.id =".$languageIdValue." AND lx.deleted =0 AND lx.active =0
                     LEFT JOIN [BILSANET_MOBILE].[dbo].[sys_specific_definitions]  ax on (ax.language_parent_id = a.[id] or ax.[id] = a.[id] ) and  ax.language_id= lx.id  
                     WHERE a.[main_group] = 1 and a.[first_group]  = 7 and
                         a.language_parent_id =0 
