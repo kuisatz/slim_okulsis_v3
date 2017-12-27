0143bd3693267303e0b14eea4785da11d0decf01
kuisatz <kuisatzhaderah@gmail.com>
2017-12-27 17:40:19 +0300

    sendMesajDefault TEST tamam

--- vendor/dal/pdo/MblLogin.php
+++ vendor/dal/pdo/MblLogin.php
@@ -3790,7 +3790,7 @@ class MblLogin extends \DAL\DalSlim {
                             SET NOCOUNT OFF;    
                                 ";
                             $statement = $pdo->prepare($sql); 
-                            echo debugPDO($sql, $params);
+                         //   echo debugPDO($sql, $params);
                          //   $result = $statement->execute();
 
                           
