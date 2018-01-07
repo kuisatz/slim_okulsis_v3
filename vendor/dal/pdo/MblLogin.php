1ed848a84eceea8d23b8a7dfff338ad1a2df91a5
kuisatz <kuisatzhaderah@gmail.com>
2018-01-07 15:44:43 +0300

    WHERE '2011-11-11' <= SNV.SinavTarihi  )

--- vendor/dal/pdo/MblLogin.php
+++ vendor/dal/pdo/MblLogin.php
@@ -5654,7 +5654,7 @@ class MblLogin extends \DAL\DalSlim {
                                         INNER JOIN ".$dbnamex."OGT_OkulOgretmenleri oo ON oo.OgretmenID = '".$KisiID."' 
                                         INNER JOIN ".$dbnamex."SNV_SinavOkullari SO ON SO.SinavID = SNV.SinavID
                                         INNER JOIN ".$dbnamex."GNL_OkulKullanicilari OK ON OK.OkulID = SO.OkulID  AND OK.KisiID = '".$KisiID."'
-                                        WHERE getdate() <= SNV.SinavTarihi)
+                                        WHERE '2011-11-11' <= SNV.SinavTarihi  ) 
                             when 'sinav/ogrenci.html' then ( 
                                 SELECT count(distinct SinavID) as adet from (
                                     SELECT  SNV.SinavID
