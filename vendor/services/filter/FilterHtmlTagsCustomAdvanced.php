4190a022412971c8c04f5a30711c13828cc4f394
kuisatz <kuisatzhaderah@gmail.com>
2018-01-12 10:46:14 +0300

    $filterChain ->ripTags($filterChain);

--- vendor/services/filter/FilterHtmlTagsCustomAdvanced.php
+++ vendor/services/filter/FilterHtmlTagsCustomAdvanced.php
@@ -51,8 +51,31 @@ class FilterHtmlTagsCustomAdvanced implements \Zend\ServiceManager\FactoryInterf
                                                ),
                         'replacement' => '',
                     ), 200));
+         $filterChain ->ripTags($filterChain);
         return $filterChain;
-
+       
     }
+    /**
+ * Works like native "strip_tags" function but prevents
+ * texts from being glued together when removing tags.
+ *
+ * @param string $str
+ * @return string
+ */
+function ripTags($str)
+{
+    // remove HTML tags
+    $str = preg_replace('/<[^>]*>/', ' ', $str);
+ 
+    // remove control characters 
+    $str = str_replace("\r", '', $str);
+    $str = str_replace("\n", ' ', $str);
+    $str = str_replace("\t", ' ', $str);
+ 
+    // remove multiple spaces 
+    $str = trim(preg_replace('/ {2,}/', ' ', $str));
+ 
+    return $str;
+}
 
 }
